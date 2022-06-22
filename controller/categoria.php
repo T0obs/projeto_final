8d678e7d34ff74cfd994ba7ea07459f82ddc0a03
Tobias Bueno Pereira <13304366601@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-22 14:59:08 -0300

    Inicio do  Projeto

---
+++ controller/categoria.php
@@ -0,0 +1,11 @@
+<?php
+
+    require "../conexao.php";
+    require "../model/categoria_model.php";
+
+    $model = new CategoriaModel($con);
+    //$model->inserir("Produto de Limpeza");
+    //$model->excluir(1);
+    $model->atualizar("Smartphone", 2);
+    var_dump($model->buscarPorid(2));
+?>
